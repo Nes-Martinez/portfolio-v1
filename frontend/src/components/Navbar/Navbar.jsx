71c6ea8b01eff4f5fb4de9aa89df976ae91d18b5
Nes-Martinez <nestor.s.martinez@outlook.com>
2022-04-30 18:58:34 -0400

    Resume added * nav bar button

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -12,12 +12,20 @@ const Navbar = ({ toggle }) => {
       </div>
       <ul className="navbar-links">
         {["home", "about", "work", "skills", "contact"].map((item) => (
-          <li className="navbar-listItem" key={`link-${item}`}>
+          <li className="navbar__listItem" key={`link-${item}`}>
             <a href={`#${item}`}>{item}</a>
             <div />
           </li>
         ))}
+        <li className="navbar__button">
+          <button>
+            <a href="/NM-resume.pdf" target="_blank" rel="noopener noreferrer">
+              Resume
+            </a>
+          </button>
+        </li>
       </ul>
+
       <div className="navbar__mobile-toggle" onClick={toggle}>
         <MenuBars />
       </div>
