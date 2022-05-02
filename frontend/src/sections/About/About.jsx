52d4ff191827a26a3e4ab1f51de836ffbee9dbbe
Nes-Martinez <nestor.s.martinez@outlook.com>
2022-05-02 13:30:25 -0400

    Sanity configucation addded, dynamic query

--- frontend/src/sections/About/About.jsx
+++ frontend/src/sections/About/About.jsx
@@ -1,39 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 import "./About.scss";
-import { images } from "../../constants";
-
-const items = [
-  {
-    title: "Web Developer",
-    description: "Hello there, how are you?",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Web Developer",
-    description: "Hello there, how are you?",
-    imgUrl: images.about02,
-  },
-  {
-    title: "Web Developer",
-    description: "Hello there, how are you?",
-    imgUrl: images.about03,
-  },
-  {
-    title: "Web Developer",
-    description: "Hello there, how are you?",
-    imgUrl: images.about04,
-  },
-];
+import { urlFor, client } from "../../client";
 
 const About = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "items"]';
+
+    async function fetchAbout() {
+      try {
+        let data = await client.fetch(query);
+        setItems(data);
+      } catch (error) {
+        return error;
+      }
+    }
+    fetchAbout();
+  }, []);
+
   return (
     <div className="app__about">
       <h2 className="section-header-text">About Me</h2>
       <div className="app__about-info-blocks">
         {items.map((item, index) => (
           <div className="app__about-block" key={item.title + index}>
-            <img src={item.imgUrl} alt={item.title}></img>
+            <img src={urlFor(item.imgUrl)} alt={item.title}></img>
             <h2 className="title-text">{item.title}</h2>
             <p className="description-text">{item.description}</p>
           </div>
